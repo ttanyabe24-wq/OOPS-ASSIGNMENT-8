4cdb5a94c31edde2801bfd2b0aa8bcd975ca73c1
ttanyabe24-wq <ttanya_be24@thapar.edu>
2026-04-25 15:50:23 +0530

    ques4.cpp

--- ques4.cpp
+++ ques4.cpp
@@ -9,11 +9,24 @@ void bubblesort(T arr[], int n) {
             if (arr[j] > arr[j + 1])
                 swap(arr[j], arr[j + 1]);
 }
-int main(){
-    int arr[]={55,44,33,66,77};
-    int n=5;
-    bubblesort(arr,n);
-     for (int i = 0; i < n; i++) cout << arr[i] << " ";
-    cout << endl;
+int main() {
+    int n;
+    int arr[100];
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bubblesort<int>(arr, n);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
